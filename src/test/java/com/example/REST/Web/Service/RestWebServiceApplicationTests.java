7c6c0a338255839fa9758af0eb8b781ad09c50c5
Nicole Mis <nicolemis@macbook-pro.myfiosgateway.com>
2022-04-19 23:27:52 -0400

    creating a REST web service in Spring Boot with Gradle

---
+++ src/test/java/com/example/REST/Web/Service/RestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.REST.Web.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
